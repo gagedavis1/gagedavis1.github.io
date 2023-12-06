7686b9d9598923684961ed847f11547d7e1229c7
Gage Davis <gagedavis1203@gmail.com>
2023-12-06 16:09:33 +0000

    Added collectibles and cannons

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -61,6 +61,12 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable('database', 100, 600, 0, 0)
+    createCollectable('diamond', 200, 500, 0, 0)
+    createCollectable('grace', 300, 400, 0, 0)
+    createCollectable('kennedi', 400, 300, 0, 0)
+    createCollectable('max', 500, 200, 0, 0)
+
 
 
 
@@ -70,6 +76,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon('top', 570, 1000, 10, 10)
+    createCannon('left', 530, 1500, 20, 15)
+    createCannon("right", 510, 2500, 20, 15)
 
 
 
